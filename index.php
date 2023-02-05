62f29062a5d6bf20464d965329e3bbea395d1c2a
kelvin2002k <123325178+kelvin2002k@users.noreply.github.com>
2023-02-05 12:46:35 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>"I Wanna Be Yours" by Arctic Monkeys</title>
+  <link rel="stylesheet" type="text/css" href="css2.css">
+</head>
+<body>
+  <header>
+    <h1>I Wanna Be Yours</h1>
+    <h2>by Arctic Monkeys:</h2>
+  </header>
+  <main>
+    <section class="lyrics">
+       <p> Here are the lyrics to "I Wanna Be Yours" by Arctic Monkeys:</p>
+
+        [Verse 1]
+        <p>Let me be your vacuum cleaner</p>
+        <p>Breathing in your dust</p>
+        <p>I wanna be your Ford Cortina</p>
+        <p>I will never rust</p>
+        
+        [Chorus]
+        <p>If you like your coffee hot</p>
+        <p>Let me be your coffee pot</p>
+        <p>You call the shots babe</p>
+        <p>I just wanna be yours</p>
+        
+        [Verse 2]
+        <p>Wanna be your vacuum cleaner</p>
+        <p>Breathing in your dust</p>
+        <p>I wanna be your Ford Cortina</p>
+        <p>I will never rust</p>
+        
+        [Chorus]
+        <p>If you like your coffee hot</p>
+        <p>Let me be your coffee pot</p>
+        <p>You call the shots babe</p>
+        <p>I just wanna be yours</p>
+        
+        [Bridge]
+        <p>Wanna be your vacuum cleaner</p>
+        <p>Breathing in your dust</p>
+        <p>I wanna be your Ford Cortina</p>
+        <p>I will never rust</p>
+        
+        [Chorus]
+        <p>If you like your coffee hot</p>
+        <p>Let me be your coffee pot</p>
+        <p>You call the shots babe</p>
+        <p>I just wanna be yours</p>
+        
+        [Outro]
+        <p>Let me be your vacuum cleaner</p>
+        <p>Breathing in your dust</p>
+        <p>Let me be your Ford Cortina</p>
+        <p>I will never rust</p>
+    </section>
+  </main>
+</body>
+</html>
+
+
